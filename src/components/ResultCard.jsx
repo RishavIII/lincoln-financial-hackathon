5071b450e77c02914b8b3b2614107d964ebe20c2
kenny <kennycs@protonmail.com>
2025-10-18 18:50:05 -0400

    Cleaned up results page

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -1,7 +1,8 @@
 export default function ResultsCard({ title, description, visible }) {
     if (visible) {
         return (
-            <div className="p-6 h-full bg-white rounded-xl shadow hover:shadow-md transition">
+            // maybe not fixed height
+            <div className="p-6 m-5 h-100  basis-1/4 w-full bg-white rounded-xl shadow hover:shadow-md transition">
                 <h3 className="font-semibold text-lg mb-2">{title}</h3>
                 <p className="text-gray-500">{description}</p>
             </div>
