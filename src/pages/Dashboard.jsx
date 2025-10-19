bb8964a137f88dbfab4d46d4b31de78f373c3894
Yash <yashpatel.eg@gmail.com>
2025-10-18 23:23:22 -0400

    Dashboard Upgrade

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,10 +2,23 @@ import Card from "../components/Card"
 
 export default function Dashboard() {
   return (
-    <div className="p-8 bg-red-300 ">
-        <div className="p-12 min-h-svh flex justify-center items-center ">
-          <div className="text-9xl">
+    <div className="p-8  ">
+        <div className="p-12 min-h-[calc(85vh)] flex justify-center items-center ">
+          {/* <div className="text-9xl text-[#4cae4f]">
          InsoraPath 
+          </div> */}
+          <div className="  px-20 flex-col">
+                      <img src="src\assets\insurapath-high-resolution-logo-transparent.png" alt="description" className=" h-[20vh] rounded-lg  " />
+
+                        <div>
+                          <div className="text-4xl text-[#c7e5c8] font-bold text-center">Your journey to the right coverage</div>
+                        </div>
+            
+                        <div className=" flex justify-center items-center  h-[20vh]">
+                         <button className="px-6 py-2 bg-[#4cae4f] text-white rounded-lg hover:bg-blue-700">
+                         Questioner
+                         </button>
+                       </div>
           </div>
         </div>
     </div>
